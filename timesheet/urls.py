19bb4730ade7af78e0bba37cf29ae19e040d039c
Iro <kocaniiro@gmail.com>
2022-04-13 14:35:15 -0400

    First Commit

---
+++ timesheet/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+
+urlpatterns=[
+    #path('',list_doctors),
+    #path('<int:user_id>/',user_detail),
+
+]
---
+++ workday_managment/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WorkdayManagmentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'workday_managment'
